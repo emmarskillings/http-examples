8574404c4d08edbaac5691055c75c2d5f3486cc3
emmarskillings <emmarskillings@gmail.com>
2018-06-11 16:33:01 +0000

    step 3 complete

--- step3.js
+++ step3.js
@@ -1,26 +1,23 @@
-function getAndPrintHTML() {
+function getAndPrintHTML(options) {
 
   var buffer = '';
   var https = require('https');
 
-  var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step2.html'
-  };
-
-  https.get(requestOptions, function(response) {
+   https.get(options, function(response) {
     response.setEncoding('utf8');
     response.on('data', function(data) {
       buffer += data;
     });
-
       response.on('end', function() {
       console.log(buffer);
     });
   });
 
-
-
 }
 
-getAndPrintHTML();
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
